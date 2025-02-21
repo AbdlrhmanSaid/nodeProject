531a858801fcbc93bcf5ca4811e511a78bbcdc75
Abdelrhman Said <119136719+AbdlrhmanSaid@users.noreply.github.com>
2025-02-21 02:05:41 +0200

    update schema

--- model/Users.js
+++ model/Users.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  email: { type: String, required: true },
+  position: {
+    type: String,
+    enum: ["normal", "owner", "admin"],
+    default: "normal",
+  },
 });
 
 const User = mongoose.model("users", userSchema);
