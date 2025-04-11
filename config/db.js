b880b863be6eb876b47c450b7641079e97f7a6bb
Abdelrhman Said <119136719+AbdlrhmanSaid@users.noreply.github.com>
2025-04-11 18:02:24 +0200

    refactor: simplify MongoDB connection by removing unnecessary password encoding

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
   try {
     // قراءة المتغيرات من ملف .env
     const db_user = process.env.DB_USER;
-    const db_password = process.env.DB_PASSWORD; // استخدم القيمة كما هي دون ترميز إضافي
+    const db_password = process.env.DB_PASSWORD;
     const db_name = process.env.DB_NAME;
 
     // إعداد رابط الاتصال بـ MongoDB بدون الخيارات deprecated
